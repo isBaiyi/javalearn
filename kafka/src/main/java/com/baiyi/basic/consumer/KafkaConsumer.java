2a080457eb3d1c579b07ed17c0b0b31051ec5638
BaiYi <1101293873@qq.com>
2022-03-29 22:32:02 +0800

    Kafka 从指定分区、offset、时间开始消费

--- kafka/src/main/java/com/baiyi/basic/consumer/KafkaConsumer.java
+++ kafka/src/main/java/com/baiyi/basic/consumer/KafkaConsumer.java
@@ -1,14 +1,15 @@
-package com.baiyi.basic.producer;
+package com.baiyi.basic.consumer;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.OffsetAndTimestamp;
+import org.apache.kafka.common.PartitionInfo;
 import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.serialization.StringDeserializer;
 
 import java.time.Duration;
-import java.util.Collections;
-import java.util.Properties;
+import java.util.*;
 
 /**
  * @author: BaiYi
@@ -45,7 +46,36 @@ public class KafkaConsumer {
         // 消费者订阅主题列表
         // consumer.subscribe(Collections.singletonList(TOPIC_NAME));
         // 从指定分区开始消费
-        consumer.assign(Collections.singleton(new TopicPartition(TOPIC_NAME, 0)));
+        // consumer.assign(Collections.singleton(new TopicPartition(TOPIC_NAME, 0)));
+
+        // 消息回溯消费
+        // consumer.assign(Collections.singleton(new TopicPartition(TOPIC_NAME, 1)));
+        // consumer.seekToBeginning(Collections.singleton(new TopicPartition(TOPIC_NAME, 1)));
+
+        // 从指定offset开始消费
+        // consumer.assign(Collections.singleton(new TopicPartition(TOPIC_NAME, 1)));
+        // consumer.seek(new TopicPartition(TOPIC_NAME, 1), 10);
+
+        // 从指定时间开始消费
+        List<PartitionInfo> topicPartitions = consumer.partitionsFor(TOPIC_NAME); // 获取当前主题的所有 partition
+        // 从一个小时前开始消费
+        long fetchTime = new Date().getTime() - 1000 * 60 * 60;
+        Map<TopicPartition, Long> topicPartitionMap = new HashMap<>();
+        for (PartitionInfo partitionInfo : topicPartitions) {
+            topicPartitionMap.put(new TopicPartition(TOPIC_NAME, partitionInfo.partition()), fetchTime);
+        }
+        Map<TopicPartition, OffsetAndTimestamp> partitionMap = consumer.offsetsForTimes(topicPartitionMap);
+        for (Map.Entry<TopicPartition, OffsetAndTimestamp> entry : partitionMap.entrySet()) {
+            TopicPartition key = entry.getKey();
+            OffsetAndTimestamp value = entry.getValue();
+            if(key == null || value == null) continue;
+            long offset = value.offset();
+            log.info("topic: {}, partition: {}, offset: {}\n", TOPIC_NAME, key.partition(), offset);
+            // 开始根据 partition 和 offset 进行消费
+            consumer.assign(Collections.singleton(key));
+            consumer.seek(key, offset);
+        }
+
 
         while (true) {
             // 每 1000 ms 拉取一次消息
@@ -59,13 +89,13 @@ public class KafkaConsumer {
             if (records.count() > 0) {//有消息
                 // ⼿动同步提交offset，当前线程会阻塞直到offset提交成功
                 // ⼀般使⽤同步提交，因为提交之后⼀般也没有什么逻辑代码了
-                // consumer.commitSync();//=======阻塞=== 提交成功
-                consumer.commitAsync((map, e) -> {
+                consumer.commitSync();//=======阻塞=== 提交成功
+                /*consumer.commitAsync((map, e) -> {
                     if (e != null) {
                         log.error("消费者手动异步提交offset失败, offsets: {}, e: {}", map, e.getMessage(), e);
                         // 执行相关操作 写异常日志....
                     }
-                });
+                });*/
             }
         }
     }
